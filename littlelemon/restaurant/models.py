58024fea5f0f14b445cab574152182a0a660647d
Hamzah Nurwarid <hn.warid@gmail.com>
2023-10-18 18:58:16 +0700

    updated model class string representation

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -7,8 +7,13 @@ class Booking(models.Model):
     no_of_guests = models.IntegerField(default=6)
     bookingDate = models.DateTimeField()
 
+    def __str__(self): 
+        return f"{self.name} for {self.no_of_guests} on {self.bookingDate}"
 
 class Menu(models.Model): 
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField(default=5)
+
+    def __str__(self):
+        return f"{self.title} : ${self.price} now {self.inventory} left in stock"
